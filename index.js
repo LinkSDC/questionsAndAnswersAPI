2df2825a74f62ac31d6a6067c842e2db43ad2ac6
hunterMotko <hunterMotko@gmail.com>
2022-04-21 22:09:14 -0400

    router setup

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const router = require('./routes/routes.js');
+require('dotenv').config();
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json())
+
+app.use('/api/fec2/rfp/qa', router);
+
+app.listen(process.env.SERVER, ()=>console.log(`Listening on ${process.env.SERVER}`));
---
+++ routes/routes.js
@@ -0,0 +1,21 @@
+const router = express.Router();
+
+// GET /qa/questions
+// query_params product_id, page, count
+router.get('')
+
+// GET /qa/questions/:question_id/answers
+// query params - count, page
+
+
+// POST /qa/questions
+
+// POST /qa/questions/:question_id/answers
+
+// PUT /qa/questions/:questions_id/helpful
+
+// PUT /qa/questions/:questions_id/report
+
+// PUT /qa/answers/:answer_id/helpful
+
+// PUT /qa/answers:answer_id/report
