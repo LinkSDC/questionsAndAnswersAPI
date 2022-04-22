df8d2d949358ce0141ddadfe2469480be14e0cbc
hunterMotko <hunterMotko@gmail.com>
2022-04-21 21:09:38 -0400

    controller model setup

--- parse.js
+++
@@ -1,8 +0,0 @@
-const { createReadStream } = require('fs');
-const path = require('path');
-const pg = require('pg');
-const csv = require('csv-parser');
-
-createReadStream('./csv/product.csv')
-  .pipe(csv())
-  .on('data', data=>console.log(data))
--- schema.sql
+++ schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS questions (
   question_id SERIAL PRIMARY KEY,
   product_id INT NOT NULL,
   question_body TEXT NOT NULL,
-  question_date BIGINT NOT NULL DEFAULT CURRENT_DATE,
+  question_date BIGINT NOT NULL,
   asker_name VARCHAR(60) NOT NULL,
   asker_email VARCHAR(60) NOT NULL,
   reported BOOLEAN DEFAULT false,
@@ -25,14 +25,14 @@ CREATE TABLE IF NOT EXISTS questions (
 );
 
 CREATE TABLE IF NOT EXISTS answers (
-  id SERIAL PRIMARY KEY,
+  answer_id SERIAL PRIMARY KEY,
   question_id INT NOT NULL,
   body TEXT NOT NULL,
-  date BIGINT NOT NULL DEFAULT CURRENT_DATE,
+  date BIGINT NOT NULL,
   answerer_name VARCHAR(60) NOT NULL,
   answerer_email VARCHAR(60) NOT NULL,
   reported BOOLEAN DEFAULT false,
-  helpful INT DEFAULT 0,
+  helpfulness INT DEFAULT 0,
   FOREIGN KEY (question_id) REFERENCES questions (question_id)
 );
 
@@ -40,5 +40,8 @@ CREATE TABLE IF NOT EXISTS answers_photos (
   id SERIAL PRIMARY KEY,
   answer_id INT NOT NULL,
   url VARCHAR(255),
-  FOREIGN KEY (answer_id) REFERENCES answers (id)
+  FOREIGN KEY (answer_id) REFERENCES answers (answer_id)
 );
+
+ALTER TABLE product ADD COLUMN created_at TIMESTAMP DEFAULT NOW();
+ALTER TABLE product ADD COLUMN updated_at TIMESTAMP DEFAULT NOW();
